298513ff27e03106e05870a0f3bafffe96a2bf7e
ericpkatz <ericpkatz@gmail.com>
2023-11-09 11:57:02 -0500

    commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+function getLength(arr){
+  return arr.length;
+}
+
+function getSum(){
+  
+}
+
+const numbers = [1, 3, 5, 8];
+console.log(`You have given ${getLength(numbers)} numbers.`);
+console.log(`The sum of your numbers is ${getSum(numbers)}.`);
+//console.log(`The mean of your numbers is ${getMean(numbers)}.`);
+//console.log(`The smallest of your numbers is ${getMin(numbers)}.`);
+//console.log(`The largest of your numbers is ${getMax(numbers)}.`);
+//console.log(`The range of your numbers is ${getRange(numbers)}.`);
+//console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
+//console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
